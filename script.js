6e9aee05fb69b1dbb50431d475277d725a4c5550
Patra <git-patra@github.com>
2020-02-15 16:09:20 +0700

    - Ngubah Hover - Nambahin Score - Nambahin acak gambar computer

--- script.js
+++ script.js
@@ -1,61 +1,71 @@
 function getCompPick() {
-    const comp = Math.random();
-  
-    if (comp < 0.34) return "gajah";
-    if (comp >= 0.34 && comp < 0.67) return "orang";
-    return "semut";
-  }
-  
-  function rules(comp, player) {
-    if (player == comp) return "SERI!";
-    if (player == "gajah") return (comp == "orang") ? "MENANG!" : "KALAH";
-    if (player == "semut") return (comp == "gajah") ? "KALAH!" : "MENANG";
-    if (player == "orang") return (comp == "semut") ? "MENANG!" : "KALAH";
-  }
-  
+  const comp = Math.round(Math.random() * 8);
+
+  if (comp < 3) return "gajah";
+  if (comp >= 3 && comp < 6) return "orang";
+  return "semut";
+}
+
 function rules(comp, player) {
-    if (player == comp) return "SERI!";
-    if (player == "gajah") return (comp == "orang") ? "MENANG!" : "KALAH";
-    if (player == "semut") return (comp == "gajah") ? "KALAH!" : "MENANG";
-    if (player == "orang") return (comp == "semut") ? "MENANG!" : "KALAH";
-  }
-  
-  function putar() {
-    const imgComp = document.querySelector('.img-comp');
-    const gambar = ['gajah', 'semut', 'orang'];
-  
-    let i = 1;
-    const waktuMulai = new Date().getTime();
-    setInterval(function () {
-      if (new Date().getTime() - waktuMulai > 1000) {
-        clearInterval;
-        return;
-      }
-      imgComp.setAttribute('src', 'img/' + gambar[i++] + '1.png');
-      if (i == gambar.length) i = 0;
-    }, 100)
-  }
-  
-  
-  const pick = document.querySelectorAll('.player img');
-  pick.forEach(function (i) {
-    i.addEventListener('click', function (e) {
+  if (comp == player) return "SERI!";
+  if (player == "gajah") return comp == "orang" ? "MENANG!" : "KALAH!";
+  if (player == "semut") return comp == "gajah" ? "KALAH!" : "MENANG!";
+  if (player == "orang") return comp == "semut" ? "MENANG!" : "KALAH!";
+}
+
+var menang = 0;
+var kalah = 0;
+var seri = 0;
+
+function acak() {
+  const imgComp = ["gajah", "orang", "semut"];
+  const waktuAwal = new Date().getTime();
+
+  let a = 1;
+
+  setInterval(function() {
+    if (new Date().getTime() - waktuAwal > 1000) {
+      clearInterval;
+      return;
+    }
+    const acakImg = document.querySelector(".img-comp");
+    acakImg.setAttribute("src", "img/" + imgComp[a++] + "1.png");
+    if (a == imgComp.length) a = 0;
+  }, 100);
+}
+
+const player = document.querySelector(".player");
+const pick = document.querySelectorAll(".player img");
+
+pick.forEach(function(i) {
+  i.addEventListener("click", function(e) {
     const pComp = getCompPick();
-    const pPlayer = i.getAttribute('alt');
-        e.target.classList.toggle('bayang');
-        console.log(e.target);
-      const hasil = rules(pComp, pPlayer);
-      putar();
-  
-      setTimeout(function () {
-        const imgComp = document.querySelector('.img-comp');
-        imgComp.setAttribute('src', 'img/' + pComp + '1.png');
-        const info = document.querySelector('.info');
-        info.innerHTML = hasil;
-        
-      }, 1000);
-  
-  
-    });
+    const pPlayer = e.target.getAttribute("alt");
+
+    e.target.classList.toggle("bayang");
+    hasil = rules(pComp, pPlayer);
+
+    acak();
+
+    const gambarComp = document.querySelector(".comp img");
+    setTimeout(function() {
+      gambarComp.setAttribute("src", "img/" + pComp + "1.png");
+      info.innerHTML = hasil;
+      menangIn.innerHTML = "You " + menang;
+      kalahIn.innerHTML = kalah + " Comp";
+    }, 1000);
+
+    const info = document.querySelector(".info");
+
+    if (hasil == "MENANG!") {
+      menang += 1;
+    } else if (hasil == "SERI!") {
+      seri += 1;
+    } else {
+      kalah += 1;
+    }
+
+    const menangIn = document.querySelector(".pscore h2");
+    const kalahIn = document.querySelector(".cscore h2");
   });
-  
+});
